6dfda2968b90eabbc4e596c3c744e108038d8f9c
mankunssanil <78522919+mankunssanil@users.noreply.github.com>
2024-03-19 13:30:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,22 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template('nextpage.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # Here you would typically perform your prediction
-        # For demonstration purposes, let's assume prediction results are stored in a list called 'prediction'
-        prediction = [0.2, 0.4, 0.1, 0.3, 0.5, 0.7, 0.6]
-        return render_template('nextpage.html', prediction=prediction, filename=filename)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['Skin-Disease'], filename))
+            # Here you would typically perform your prediction
+            # For demonstration purposes, let's assume prediction results are stored in a list called 'prediction'
+            prediction = [0.2, 0.4, 0.1, 0.3, 0.5, 0.7, 0.6]
+            return render_template('index.html', prediction=prediction, filename=filename)
+    return redirect(url_for('nextpage.html'))  # Redirect to index if GET method is used
 
 if __name__ == '__main__':
     app.run(debug=True)
