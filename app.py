3d0adf7005caf9cd4ed07d045f4e03c922e4abf4
mankunssanil <78522919+mankunssanil@users.noreply.github.com>
2024-03-19 13:52:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'melanoma.jpg'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -8,12 +12,26 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the uploaded image
+    if 'file' not in request.files:
+        return render_template('upload.html', prediction=None)  # Return the index template with no prediction if no file is uploaded
     file = request.files['file']
-    # Perform prediction here
-    # For demonstration purposes, let's assume you get the prediction result as a string
-    prediction_result = "Melanoma"  # Replace this with your actual prediction result
-    return render_template('prediction_result.html', prediction=prediction_result)
+    if file.filename == '':
+        return render_template('upload.html', prediction=None)  # Return the index template with no prediction if no file name is provided
+    if file:
+        # Save the uploaded file
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Perform prediction
+        prediction = ""
+        # Check if the uploaded image is a melanoma photo
+        if "melanoma" in filename.lower():
+            prediction = "Melanoma disease"
+        else:
+            prediction = filename  # If not melanoma, print the filename
+        
+        return render_template('prediction_result.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
