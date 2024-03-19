0aa209f69f3bdb0ba71e4873b97dce0a817b4024
mankunssanil <78522919+mankunssanil@users.noreply.github.com>
2024-03-19 11:50:25 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,26 @@
+import os
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
-@app.router('/', methods=["GET", "POST"])
-def upload_predict():
-    return render_template("index.html")
+UPLOAD_FOLDER = "Skin-Disease"
+MODEL = None
 
-if __name__=="__main__":
+@app.router("/", methods=["GET", "POST"])
+def upload_predict():
+    if request.method == "POST":
+    image_file = request.files["image"]
+    if imade_file:
+        image_location = os.path.join(
+            UPLOAD_FOLDER,
+            image_file.filename
+        )
+        image_file.save(image_location)
+        return render_template("nextpage.html", prediction=1)
+    return render_template("nextpage.html", prediction=0)
+    
+
+if __name__ == "__main__":
     app.run(port=12000, debug=True)
 
